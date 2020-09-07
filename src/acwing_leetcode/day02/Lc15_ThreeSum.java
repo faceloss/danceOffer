8ff37da2b9899bbfecf62e062bbf5cf222369d1f
kazi <chuanwei_yin@126.com>
2020-09-07 23:31:12 +0800

    3sum

--- src/acwing_leetcode/day02/Lc15_ThreeSum.java
+++ src/acwing_leetcode/day02/Lc15_ThreeSum.java
@@ -25,25 +25,42 @@ import java.util.List;
 
 public class Lc15_ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        if(nums == null || nums.length==0){
-            return null;
-        }
-        HashMap map = new HashMap();
+        int n = nums.length;
         Arrays.sort(nums);
-        // -4 -1 -1 0 1 2
-        int len = nums.length;
-        int left = 0;
-        int right = len - 1;
-        while(left < right){
-            if(map.containsKey(0-nums[left]-nums[right])){
-                List<Integer> cur = new ArrayList<>();
-                cur.add(nums[left]);
-                cur.add(nums[left]);
-                cur.add(0);
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        // 枚举 a
+        for (int first = 0; first < n; ++first) {
+            // 需要和上一次枚举的数不相同
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            // c 对应的指针初始指向数组的最右端
+            int third = n - 1;
+            int target = -nums[first];
+            // 枚举 b
+            for (int second = first + 1; second < n; ++second) {
+                // 需要和上一次枚举的数不相同
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+                // 需要保证 b 的指针在 c 的指针的左侧
+                while (second < third && nums[second] + nums[third] > target) {
+                    --third;
+                }
+                // 如果指针重合，随着 b 后续的增加
+                // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+                if (second == third) {
+                    break;
+                }
+                if (nums[second] + nums[third] == target) {
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(nums[first]);
+                    list.add(nums[second]);
+                    list.add(nums[third]);
+                    ans.add(list);
+                }
             }
         }
-
-        return null;
+        return ans;
     }
 }
